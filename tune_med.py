3e368a29c9d00f9256e222a6a494cd5a5328744a
Jesse Liu <jesseliu2000@gmail.com>
2024-10-28 06:53:16 +0000

    init success

--- tune_med.py
+++ tune_med.py
@@ -12,7 +12,7 @@ from datasets import load_dataset
 dataset = load_dataset(
     'json',
     data_files='oneround.jsonl',  
-)
+)['train']
 
 train_val_test_split = dataset.train_test_split(test_size=0.2, seed=42) 
 val_test_split = train_val_test_split['test'].train_test_split(test_size=0.5, seed=42) 
