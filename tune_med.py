34a4c7d1d62c913aa851f2db6cc5eebb05c9d020
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2024-10-28 01:43:17 -0500

    Update tune_med.py

--- tune_med.py
+++ tune_med.py
@@ -11,9 +11,8 @@ from datasets import load_dataset
 
 dataset = load_dataset(
     'json',
-    data_files={'all': 'oneround.jsonl'},  
-    split='all',
-    json={'lines': True}  
+    data_files={'all': 'your_dataset.jsonl'},  
+    split='all'  
 )
 
 train_val_test_split = dataset.train_test_split(test_size=0.2, seed=42) 
