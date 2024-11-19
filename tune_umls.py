92371eaccfe6cb369095e7889bfd6df1fafbb279
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2024-11-19 16:26:32 -0600

    Update tune_umls.py

--- tune_umls.py
+++ tune_umls.py
@@ -102,8 +102,8 @@ if test_dataset:
 training_args = TrainingArguments(
     output_dir="./llama_qa_results",
     num_train_epochs=3,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
     gradient_accumulation_steps=2,
     evaluation_strategy="epoch",
     save_strategy="epoch",
