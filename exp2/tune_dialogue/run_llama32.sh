b13a1c48d9fd6b96eaf5c13470e8b158ba0eb901
jesseliu0913 <l.zijie@wustl.edu>
2024-12-27 20:23:50 -0500

    update scripts

--- exp2/tune_dialogue/run_llama32.sh
+++ exp2/tune_dialogue/run_llama32.sh
@@ -2,7 +2,7 @@
 export NCCL_P2P_DISABLE=1
 
 LOG_FOLDER="./logs"
-LOG_FILE="$LOG_FOLDER/tune_context.log"
+LOG_FILE="$LOG_FOLDER/tune_dialogue.llama32"
 
 if [ ! -d "$LOG_FOLDER" ]; then
     echo "Log folder does not exist. Creating one..."
@@ -11,7 +11,7 @@ else
     echo "Log folder exists."
 fi
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup torchrun --master_port=29501 --nproc_per_node=4 tune_context.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup torchrun --master_port=29501 --nproc_per_node=4 tune_dialogue.py \
     --model "meta-llama/Llama-3.2-3B-Instruct" \
     --epoch 2 \
     --task "llama3.2" \
--- exp2/tune_dialogue/run_qwen.sh
+++ exp2/tune_dialogue/run_qwen.sh
@@ -2,7 +2,7 @@
 export NCCL_P2P_DISABLE=1
 
 LOG_FOLDER="./logs"
-LOG_FILE="$LOG_FOLDER/tune_context.qwen"
+LOG_FILE="$LOG_FOLDER/tune_dialogue.qwen"
 
 if [ ! -d "$LOG_FOLDER" ]; then
     echo "Log folder does not exist. Creating one..."
@@ -11,7 +11,7 @@ else
     echo "Log folder exists."
 fi
 
-CUDA_VISIBLE_DEVICES=4,6,7 nohup torchrun --nproc_per_node=3 tune_context.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 nohup torchrun --nproc_per_node=4 tune_dialogue.py \
     --model "Qwen/Qwen2.5-3B-Instruct" \
     --epoch 2 \
     --task "qwen2.5" \
