9058e7fe701c3a6ae0cc9d34c792d5e8e890c8e5
jesseliu0913 <l.zijie@wustl.edu>
2024-12-28 20:55:49 -0500

    update tune dialogue exp1

---
+++ exp1/tune_dialogue/run_llama32.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export NCCL_P2P_DISABLE=1
+
+LOG_FOLDER="./logs"
+LOG_FILE="$LOG_FOLDER/tune_dialogue.llama32"
+
+if [ ! -d "$LOG_FOLDER" ]; then
+    echo "Log folder does not exist. Creating one..."
+    mkdir -p "$LOG_FOLDER"
+else
+    echo "Log folder exists."
+fi
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup torchrun --master_port=29501 --nproc_per_node=4 tune_umls_dialogue.py \
+    --model "meta-llama/Llama-3.2-3B-Instruct" \
+    --epoch 2 \
+    --task "llama3.2" \
+    --batch_size 2 \
+    --learning_rate 5e-5 \
+    --max_length 2048 > "$LOG_FILE" 2>&1 &
--- exp1/tune_dialogue/tune_umls_dialogue.py
+++ exp1/tune_dialogue/tune_umls_dialogue.py
@@ -1,6 +1,8 @@
+import os
 import json
 import torch
 import random
+import argparse
 from torch.utils.data import IterableDataset
 from torch.utils.data import Dataset, DataLoader
 from transformers import (
@@ -13,11 +15,22 @@ from transformers import (
 from peft import PeftModel, LoraConfig, TaskType, PeftConfig, get_peft_model
 
 
+parser = argparse.ArgumentParser(description="Casual Tuning based on case report")
+parser.add_argument("--model", type=str, default=None, help="Set model weights")
+parser.add_argument("--epoch", type=int, default=3, help="Set Epoch")
+parser.add_argument("--task", type=str, default="llama3.2", help="Set Task Name")
+parser.add_argument("--batch_size", type=int, default=4, help="Set Batch Size")
+parser.add_argument("--max_length", type=int, default=2048, help="Set max_length")
+parser.add_argument("--learning_rate", type=float, default=1e-5, help="Set Learning Rate")
+
+args = parser.parse_args()
+
 class ConversationDataset(Dataset):
-    def __init__(self, file_path, tokenizer, split="train", val_split_ratio=0.1, seed=42):
+    def __init__(self, file_path, tokenizer, split="train", val_split_ratio=0.1, seed=42, max_length=2048):
         self.file_path = file_path
         self.tokenizer = tokenizer
         self.split = split
+        self.max_length = max_length
         self.val_split_ratio = val_split_ratio
         self.seed = seed
         self.examples = []
@@ -47,12 +60,12 @@ class ConversationDataset(Dataset):
                     inputs = self.tokenizer(
                         full_text,
                         truncation=True,
-                        max_length=512,
+                        max_length=self.max_length,
                     )
                     input_tokens = self.tokenizer(
                         input_text,
                         truncation=True,
-                        max_length=512,
+                        max_length=self.max_length,
                     )
                     input_length = len(input_tokens['input_ids'])
                     labels = inputs['input_ids'].copy()
@@ -87,15 +100,14 @@ class ConversationDataset(Dataset):
 
 
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained(args.model)
 tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+model = AutoModelForCausalLM.from_pretrained(args.model)
+model.gradient_checkpointing_enable()  
 
 file_path = './data/clean_dialogue_llama.jsonl'  
-train_dataset = ConversationDataset(file_path, tokenizer, split="train")
-val_dataset = ConversationDataset(file_path, tokenizer, split="val")
-
-
+train_dataset = ConversationDataset(file_path, tokenizer, split="train", max_length=args.max_length)
+val_dataset = ConversationDataset(file_path, tokenizer, split="val", max_length=args.max_length)
 
 peft_config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
@@ -105,10 +117,6 @@ peft_config = LoraConfig(
     lora_dropout=0.1
 )
 model = get_peft_model(model, peft_config)
-
-# lora_weights_path = "JesseLiu/umls_dialogue"
-# model = PeftModel.from_pretrained(model, lora_weights_path)
-# model.train()
 
 
 class CustomDataCollatorWithPadding(DataCollatorWithPadding):
@@ -124,14 +132,6 @@ class CustomDataCollatorWithPadding(DataCollatorWithPadding):
         return batch
 
 data_collator = CustomDataCollatorWithPadding(tokenizer=tokenizer)
-# data_collator = DataCollatorWithPadding(tokenizer=tokenizer, padding='longest')
-
-# data_loader = DataLoader(
-#     train_dataset,
-#     batch_size=4, 
-#     collate_fn=data_collator 
-# )
-
 
 fsdp_config = {
     "fsdp_min_num_params": 20000,
@@ -139,16 +139,20 @@ fsdp_config = {
     "fsdp_sharding_strategy": "FULL_SHARD",
 }
 
+folder_path = "./model_weights"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
 training_args = TrainingArguments(
-    output_dir="./llama_dialogue_results",
-    num_train_epochs=5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    output_dir=f"{folder_path}/{args.task}_inter",
+    num_train_epochs=args.epoch,
+    per_device_train_batch_size=args.batch_size,
+    per_device_eval_batch_size=args.batch_size,
     gradient_accumulation_steps=2,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     logging_steps=100,
-    learning_rate=5e-4,
+    learning_rate=args.learning_rate,
     warmup_ratio=0.1,
     weight_decay=0.1,
     max_grad_norm=1.0,
@@ -156,6 +160,7 @@ training_args = TrainingArguments(
     adam_beta1=0.9,
     adam_beta2=0.95,
     adam_epsilon=1e-5,
+    gradient_checkpointing=True,
     ddp_backend='nccl',
     fp16=False, 
     bf16=True, 
@@ -178,5 +183,6 @@ trainer = Trainer(
 
 
 trainer.train()
-trainer.save_model("dialogue_llama8b_umls")
+trainer.save_model(f"{folder_path}/{args.task}_final")
+
 
